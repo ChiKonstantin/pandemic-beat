5bc95c3d62c0fd588b12c58ddc7df83e08731bd8
ChiKonstantin <89655575+ChiKonstantin@users.noreply.github.com>
2023-01-29 15:33:40 -0600

    Update index.js

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -3,28 +3,4 @@ const generateWav = require('../generateWav');
 
 //apis here...
 
-router.post('/data-for-wav', async (req, res, next) => {
-	try {
-		console.log('API HiT!!!!');
-		console.log('REQ BODY', req.body);
-		const allCountriesCasesArr = req.body;
-
-		for (let i = 0; i < allCountriesCasesArr.length; i++) {
-			const response = await generateWav(allCountriesCasesArr[i]);
-		}
-
-		// let i = 0;
-		// function loopGenerate() {
-		// 	setTimeout(() => {
-		// 		generateWav(allCountriesCasesArr[val]);
-
-		// 	}, 2000)
-		// }
-		res.sendStatus(200);
-		console.log('WAV file should have been generated');
-	} catch (error) {
-		next(error);
-	}
-});
-
 module.exports = router;
