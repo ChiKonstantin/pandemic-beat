4b1dbe585e0e14d58e7377a567db1d6319f77517
ChiKonstantin <89655575+ChiKonstantin@users.noreply.github.com>
2023-01-30 15:24:49 -0600

    Update Player.js

--- client/components/Player.js
+++ client/components/Player.js
@@ -58,7 +58,7 @@ export default function Player(props) {
 			>
 // 				<source
 // 					src={`https://storage.googleapis.com/pandemic_beat_wavs/sound_files_${type}/${country}_pandemic_beat${fileEnding}.wav`}
-// 					type='audio/wav'
+					type='audio/wav'
 // 				/>
 				<source
 					src={`https://storage.googleapis.com/pandemic_beat_wavs/sound_files_${type}_mp3/${country}_pandemic_beat${fileEnding}.mp3`}
